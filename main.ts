e603e8ca7d5fd7454a7471821bd782a6e8560f7e
donfii <130453085+donfii@users.noreply.github.com>
2023-05-30 11:55:11 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+    basic.showString("ºC")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(input.temperature() + 32)
+    basic.showString("ºF")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature() + 273)
+    basic.showString("K")
+    basic.clearScreen()
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
